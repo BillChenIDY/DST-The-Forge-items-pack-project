dd529ddcf2586d72657be4aa618333d542bdc4a7
Matthieu <aireayquaza@hotmail.fr>
2017-12-04 14:37:31 +0100

    Modicon

--- modinfo.lua
+++ modinfo.lua
@@ -1,14 +1,14 @@
 name        = "The Forge Items Pack"
 description = "Add DST The Forge event's items to the game."
 author      = "Aire Ayquaza"
-version     = "1.0.0"
+version     = "0.1.0a"
 
 forumthread = ""
 
 api_version = 10
 
---icon_atlas = "modicon.xml"
---icon = "modicon.tex"
+icon_atlas = "modicon.xml"
+icon       = "modicon.tex"
 
 all_clients_require_mod = true
 client_only_mod = false
